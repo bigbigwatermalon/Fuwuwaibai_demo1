12b5586a5e42bb019385da79287fdf3857ade0a2
bigbigwatermalon <1359395518@qq.com>
2020-04-02 18:42:35 +0800

    update a new model of pickups and deliveries problem

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fuwuwaibai_demo1
+## 环境配置
+pip install -r requirements.txt
+
+## VRP_P.py
+VRP模型类
+参数说明
+- distance_matrix:距离矩阵
+- demands:列表，各配送点需求
+- dep_starts:列表，货车出发点
+- dep_ends:列表，货车行驶终点
+- depot:整数，起始点唯一时的起始点，它的默认值为-1，当它不为-1时，dep_starts和dep_ends参数失效。
+- time_limit:整数（秒） 超过这个时间则叫停算法，返回结果
+- travel_time_limit:货车一趟最多行驶时间
+- discharge_time:货车在送货点卸货时间
+- vehicle_speed:货车行驶速度
+- max_travel_distance:货车一趟最多行驶路程
+
+## demo.py
+变量与其他文件中的一致，文件中考虑了三种情况，在注释中写明。
+## test1.py
+变量说明
+- b_loc:读取csv文件中的坐标
+- b_dist:将b_loc中的坐标转化为距离邻接矩阵
+- b_car:读取csv文件中车辆数量信息以及车载容量信息
+- starts:列表，设定每一辆车子的出发点
+- ends:列表，设定每一辆车子的行驶终点，这里设置其与start相同，以表示出发后回到原点。
+
+此demo目前考虑的情况为给定车辆数量，和车载重量，以及配送中心点，根据路程最短原则来规划最佳路线。  
+运行test1.py文件可输出结果，以及在自动生成的routing.png查看结果
--- demo.py
+++ demo.py
@@ -5,6 +5,7 @@ from scipy.spatial.distance import squareform
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def draw():
     for i, row in b_loc.iterrows():
         edgecolor = colors[1] if i in result['starts'] else colors[0]
@@ -17,7 +18,7 @@ def draw():
             fontsize=15, ha='center', va='center'
         )
     for i, route in enumerate(result['routes']):
-        for j in range(len(route)-1):
+        for j in range(len(route) - 1):
             loc_start = b_loc.loc[route[j]]
             loc_end = b_loc.loc[route[j + 1]]
             pos_start = loc_start[1:3].values
@@ -34,6 +35,7 @@ def draw():
     plt.gca().axis('off')
     plt.savefig('routing.png')
 
+
 if __name__ == '__main__':
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
     b_loc = pd.read_csv('BasicLocations.csv')
@@ -43,55 +45,81 @@ if __name__ == '__main__':
     只给定限载的约束，这时，限制为一个配送中心，输入参数为 depot, vehicle_capacities，其中vehicle_capacities 内元素unique
     只给定车子数量，输入参数为 dep_starts, dep_ends, num_vehicles
     '''
-# Case1:车子限载和数量约束，这时候给出每一辆车的出发点starts和终点ends，以及车子限载b_capa
-    b_capa = [14, 14, 16, 16]
-    starts = [0, 0, 1, 1]
-    ends = starts
+    # # Case1:车子限载和数量约束，这时候给出每一辆车的出发点starts和终点ends，以及车子限载b_capa
+    #     b_capa = [14, 14, 16, 16]
+    #     starts = [0, 0, 1, 1]
+    #     ends = starts
+    #     vrp = VRP(
+    #         distance_matrix=b_dist, demands=b_loc['demand'], dep_starts=starts, dep_ends=ends,
+    #         num_vehicles=len(b_capa),
+    #         vehicle_capacities=b_capa,
+    #         travel_time_limit=int(1e9),
+    #         vehicle_speed=int(1e9),
+    #         max_travel_distance=3000
+    #     )
+    #     vrp.solve()
+    #     result = vrp.result()
+    #     print(result['routes'])
+    #     # print(result)
+    #     draw()
+    #
+    # # Case2:车子数量的约束，这时候给出每一辆车的出发点starts和终点ends
+    #     b_capa = [14, 14, 16, 16]
+    #     starts = [0, 0, 1, 1]
+    #     ends = starts
+    #     vrp = VRP(
+    #         distance_matrix=b_dist, demands=b_loc['demand'], dep_starts=starts, dep_ends=ends,
+    #         num_vehicles=len(b_capa),
+    #         # vehicle_capacities=b_capa,
+    #         travel_time_limit=int(1e9),
+    #         vehicle_speed=int(1e9),
+    #         max_travel_distance=3000
+    #     )
+    #     vrp.solve()
+    #     result = vrp.result()
+    #     print(result['routes'])
+    #     # print(result)
+    #     draw()
+    #
+    # # Case3:只有限载的约束b_capa，这时，只考虑同一起始点，b_capa为载重列表，列表中数据唯一
+    #     b_capa = [14, 16]
+    #     depot = 0 ## 起始点为0
+    #     vrp = VRP(
+    #         distance_matrix=b_dist, demands=b_loc['demand'], depot=depot,
+    #         # num_vehicles=len(b_capa),
+    #         vehicle_capacities=b_capa,
+    #         travel_time_limit=int(1e9),
+    #         vehicle_speed=int(1e9),
+    #         max_travel_distance=3000
+    #     )
+    #     vrp.solve()
+    #     result = vrp.result()
+    #     print(result['routes'])
+    #     # print(result)
+    #     draw()
+    # Case4:外卖情形
+    starts = [1, 6]
+    pickups_deliveries = [
+        [2, 10],
+        [4, 3],
+        [5, 9],
+        [7, 8],
+        [15, 11],
+        [13, 12],
+        [16, 14],
+    ]
+    # print(b_dist)
     vrp = VRP(
-        distance_matrix=b_dist, demands=b_loc['demand'], dep_starts=starts, dep_ends=ends,
-        num_vehicles=len(b_capa),
-        vehicle_capacities=b_capa,
-        travel_time_limit=int(1e9),
-        vehicle_speed=int(1e9),
-        max_travel_distance=3000
+        distance_matrix=b_dist, dep_starts=starts, pickups_deliveries=pickups_deliveries,
+        num_vehicles=len(starts),
+        max_travel_distance=3000,
+        pick_capacities=1,
+        # pick_capacities=[1, 1, 1, 1, 1, 1, 1]
+        weight_limits=[3, 3]
     )
     vrp.solve()
-    result = vrp.result()
-    print(result['routes'])
+    # result = vrp.result()
+    # print(result['routes'])
     # print(result)
-    draw()
 
-# Case2:车子数量的约束，这时候给出每一辆车的出发点starts和终点ends
-    b_capa = [14, 14, 16, 16]
-    starts = [0, 0, 1, 1]
-    ends = starts
-    vrp = VRP(
-        distance_matrix=b_dist, demands=b_loc['demand'], dep_starts=starts, dep_ends=ends,
-        num_vehicles=len(b_capa),
-        # vehicle_capacities=b_capa,
-        travel_time_limit=int(1e9),
-        vehicle_speed=int(1e9),
-        max_travel_distance=3000
-    )
-    vrp.solve()
-    result = vrp.result()
-    print(result['routes'])
-    # print(result)
-    draw()
-
-# Case3:只有限载的约束b_capa，这时，只考虑同一起始点，b_capa为载重列表，列表中数据唯一
-    b_capa = [14, 16]
-    depot = 0 ## 起始点为0
-    vrp = VRP(
-        distance_matrix=b_dist, demands=b_loc['demand'], depot=depot,
-        # num_vehicles=len(b_capa),
-        vehicle_capacities=b_capa,
-        travel_time_limit=int(1e9),
-        vehicle_speed=int(1e9),
-        max_travel_distance=3000
-    )
-    vrp.solve()
-    result = vrp.result()
-    print(result['routes'])
-    # print(result)
-    draw()
+    # draw()
